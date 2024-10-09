7bcaade24867b3fb45e84ef4b7c033796c7af939
QED137 <janmajay@iitdalumni.com>
2024-10-09 14:48:16 +0200

    config file updated

--- config/keys.py
+++
@@ -1,9 +0,0 @@
-Api_key = "172b18f956a2c3db37af6eb7854e27dd"
-Air_API="687292277emsh6620811a3972b04p1a4ee9jsn8c02f9bc139b"
-
-
-user = "root"
-password = "mysql"
-host = "127.0.0.1"
-database = "weatherDB"
-port = 3306
